7163a86614c78f74acaf62d8587a0dab2bac18b1
Kokovin A.N <rycarok@gmail.com>
2021-06-26 02:35:03 +0500

    фикс

--- publish.sh
+++ publish.sh
@@ -1,9 +1,9 @@
-rm -fr ./publish 
-mkdir ./publish 
+rm -fr ./docs 
+mkdir ./docs 
 rm -fr ./dist.browser
 ./node_modules/.bin/tsc -p tsconfig.browser.json
-cp -r ./public/* ./publish
-cp -r ./dist.browser ./publish/js/environment
+cp -r ./public/* ./docs
+cp -r ./dist.browser ./docs/js/environment
 
 fix_file () {
     sed -i -E 's/(import .* from ).(.*)(.);/\1"\2.js";/' $1
@@ -16,10 +16,10 @@ fix_dir () {
     done
 }
 
-fix_dir "./publish/js/environment/*.js"
-fix_dir "./publish/js/environment/agents/*.js"
-fix_dir "./publish/js/environment/data/*.js"
-fix_dir "./publish/js/environment/data/material/*.js"
-fix_dir "./publish/js/environment/data/process/*.js"
-fix_dir "./publish/js/environment/data/types/*.js"
-fix_dir "./publish/js/environment/interfaces/*.js"
+fix_dir "./docs/js/environment/*.js"
+fix_dir "./docs/js/environment/agents/*.js"
+fix_dir "./docs/js/environment/data/*.js"
+fix_dir "./docs/js/environment/data/material/*.js"
+fix_dir "./docs/js/environment/data/process/*.js"
+fix_dir "./docs/js/environment/data/types/*.js"
+fix_dir "./docs/js/environment/interfaces/*.js"
